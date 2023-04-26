917d5cf57e9527dc9f5e45ac994c335a57b2da83
BrianZ26 <brian.zhao026@gmail.com>
2023-04-26 14:01:00 -0400

    A

--- ernest-brian/src/components/api-data.vue
+++ ernest-brian/src/components/api-data.vue
@@ -1,12 +1,41 @@
+<template>
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
+</template>
+
 <script>
-let api = []
-async function getDog() {
-  const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-  const data = await response.json()
-  api.value = data
-  console.log(data)
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted() {
+    this.loaded = false
+
+    try {
+      const dogBite = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
+      this.chartdata = dogBite
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
 }
-getDog()
 </script>
-<template></template>
-<style scoped></style>
