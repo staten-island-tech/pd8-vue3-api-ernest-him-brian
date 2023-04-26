c2480c7e9758e4821d97692a07d2514303ad6b92
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2023-04-26 14:03:41 -0400

    scissors

--- ernest-brian/src/components/TheWelcome.vue
+++ ernest-brian/src/components/TheWelcome.vue
@@ -1,11 +1,16 @@
 <script setup>
 import WelcomeItem from './WelcomeItem.vue'
+import GraphIcon from './icons/IconCommunity.vue'
+import DogIcon from './icons/IconDocumentation.vue'
+import GenderIcon from './icons/IconEcosystem.vue'
+import BoroughIcon from './icons/IconSupport.vue'
+import ScissorsIcon from './icons/IconTooling.vue'
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <GraphIcon />
     </template>
     <template #heading>Graphs</template>
     Graphs that were used include: bar graphs, pie charts, and polar area charts.
@@ -13,7 +18,7 @@ import WelcomeItem from './WelcomeItem.vue'
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <DogIcon />
     </template>
     <template #heading>Dog Breed Data</template>
     In a bar graph we display the dog breeds that bit people.
@@ -21,28 +26,25 @@ import WelcomeItem from './WelcomeItem.vue'
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
+      <GenderIcon  />
     </template>
     <template #heading>Dog Gender Data</template>
-  In a polar area chart we display the dog genders that bit people.
+    In a polar area chart we display the dog genders that bit people.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <CommunityIcon />
+      <BoroughIcon />
     </template>
     <template #heading>Borough Data</template>
-In a bar graph we display the boroughs that the incidents occured in.
+    In a bar graph we display the boroughs that the incidents occured in.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <SupportIcon />
+      <ScissorsIcon />
     </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    <template #heading>Neuter Data</template>
+    In a pie chart we display whether or not the dogs were neutered.
   </WelcomeItem>
 </template>
