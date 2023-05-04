0e7bf47a2943a3a1e8b73b8c4d19f82588063fdd
BrianZ26 <brian.zhao026@gmail.com>
2023-05-03 22:10:46 -0400

    A

--- ernest-brian/src/components/api-data.vue
+++ ernest-brian/src/components/api-data.vue
@@ -1,39 +1,113 @@
-<template>
-  <div class="container">
-    <Bar v-if="loaded" :data="chartData" />
-  </div>
-</template>
 <script>
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import Chart from 'chart.js/auto'
+import { onMounted } from 'vue'
+async function genderData() {
+  async function CheckM() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
+    const info = await response.json()
+    let male = 0
+    info.forEach((dog) => {
+      if (dog.gender.includes('M')) {
+        male++
+      }
+    })
+    console.log(male)
+    return male
+  }
+  async function CheckF() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
+    const info = await response.json()
+    let female = 0
+    info.forEach((dog) => {
+      if (dog.gender.includes('F')) {
+        female++
+      }
+    })
+    console.log(female)
+    return female
+  }
+  async function CheckU() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
+    const info = await response.json()
+    let unknown = 0
+    info.forEach((dog) => {
+      if (dog.gender.includes('U')) {
+        unknown++
+      }
+    })
+    console.log(unknown)
+    return unknown
+  }
+  CheckM()
+  CheckF()
+  CheckU()
+}
 
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data: () => ({
-    loaded: false,
-    chartData: null
-  }),
-  async mounted() {
-    this.loaded = false
+async function genderChart() {
+  const [male, female, unknown] = await genderData()
+  const labels = ['Male', 'Female', 'Unknown']
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Dog Bite by Gender Data',
+        data: [30, 30, 30]
+      }
+    ]
+  }
+}
 
-    try {
-      const { dogBite } = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-      this.chartdata = dogBite
-
-      this.loaded = true
-    } catch (e) {
-      console.error(e)
+const data = {
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  datasets: [
+    {
+      label: 'Weekly Sales',
+      data: [18, 12, 6, 9, 12, 3, 9],
+      backgroundColor: [
+        'rgba(255, 26, 104, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(0, 0, 0, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 26, 104, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(0, 0, 0, 1)'
+      ],
+      borderWidth: 1
+    }
+  ]
+}
+const config = {
+  type: 'bar',
+  data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
     }
   }
 }
+onMounted(() => {
+  const myChart = new Chart(document.getElementById('myChart'), config)
+})
 </script>
+<template>
+  <div class="about">
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  max-width: 400px;
+}
+</style>
--- ernest-brian/src/views/AboutView.vue
+++ ernest-brian/src/views/AboutView.vue
@@ -1,54 +1,51 @@
 <script>
-import { Bar } from 'vue-chartjs'
-async function CheckM() {
-  const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-  const info = await response.json()
-  let male = 0
-  info.forEach((dog) => {
-    if (dog.gender.includes('M')) {
-      male++
-    }
-  })
-  console.log(male)
-  return male
+import { Chart } from 'chart.js'
+async function genderData() {
+  async function CheckM() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
+    const info = await response.json()
+    let male = 0
+    info.forEach((dog) => {
+      if (dog.gender.includes('M')) {
+        male++
+      }
+    })
+    console.log(male)
+    return male
+  }
+  async function CheckF() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
+    const info = await response.json()
+    let female = 0
+    info.forEach((dog) => {
+      if (dog.gender.includes('F')) {
+        female++
+      }
+    })
+    console.log(female)
+    return female
+  }
+  async function CheckU() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
+    const info = await response.json()
+    let unknown = 0
+    info.forEach((dog) => {
+      if (dog.gender.includes('U')) {
+        unknown++
+      }
+    })
+    console.log(unknown)
+    return unknown
+  }
+  CheckM()
+  CheckF()
+  CheckU()
 }
-async function CheckF() {
-  const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-  const info = await response.json()
-  let female = 0
-  info.forEach((dog) => {
-    if (dog.gender.includes('F')) {
-      female++
-    }
-  })
-  console.log(female)
-  return female
-}
-async function CheckU() {
-  const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-  const info = await response.json()
-  let unknown = 0
-  info.forEach((dog) => {
-    if (dog.gender.includes('U')) {
-      unknown++
-    }
-  })
-  console.log(unknown)
-  return unknown
-}
-CheckM()
-CheckF()
-CheckU()
+genderData()
 </script>
 <template>
-  <div class="about">
-    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
-    <h1>Test</h1>
+  <div class="chartBox">
+    <canvas id="myChart" width="400" height="400"></canvas>
+    <h1>gender</h1>
   </div>
 </template>
-
-<style scoped>
-.about {
-  max-width: 400px;
-}
-</style>
