35e405f64ac98c33c43443d1eed61c12bbfc5e9e
BrianZ26 <brian.zhao026@gmail.com>
2023-05-04 13:04:06 -0400

    A

--- ernest-brian/src/views/AboutView.vue
+++ ernest-brian/src/views/AboutView.vue
@@ -1,51 +1,17 @@
-<script>
-import { Chart } from 'chart.js'
-async function genderData() {
-  async function CheckM() {
-    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-    const info = await response.json()
-    let male = 0
-    info.forEach((dog) => {
-      if (dog.gender.includes('M')) {
-        male++
-      }
-    })
-    console.log(male)
-    return male
-  }
-  async function CheckF() {
-    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-    const info = await response.json()
-    let female = 0
-    info.forEach((dog) => {
-      if (dog.gender.includes('F')) {
-        female++
-      }
-    })
-    console.log(female)
-    return female
-  }
-  async function CheckU() {
-    const response = await fetch('https://data.cityofnewyork.us/resource/rsgh-akpg.json')
-    const info = await response.json()
-    let unknown = 0
-    info.forEach((dog) => {
-      if (dog.gender.includes('U')) {
-        unknown++
-      }
-    })
-    console.log(unknown)
-    return unknown
-  }
-  CheckM()
-  CheckF()
-  CheckU()
-}
-genderData()
-</script>
 <template>
-  <div class="chartBox">
-    <canvas id="myChart" width="400" height="400"></canvas>
-    <h1>gender</h1>
-  </div>
+  <BarChart class="graph" />
 </template>
+
+<script>
+import BarChart from '../components/chart-data.vue'
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+</script>
+<style scoped>
+.graph {
+  max-width: 500px;
+}
+</style>
